8f4c0cf658fa63a65352d45469152c1f40d0c3d4
FelipeBDuoc <101734777+FelipeBDuoc@users.noreply.github.com>
2022-12-14 01:10:51 -0300

    Add files via upload

---
+++ src/pages/card/card.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.page.html',
+  styleUrls: ['./card.page.scss'],
+})
+export class CardPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
